de963ece096b61b4f68146c3d1ebfa0f601f2142
EC2 Default User <ec2-user@ip-172-31-5-135.us-east-2.compute.internal>
2018-05-27 15:47:46 +0000

    ex10

--- helloworld/application.py
+++ helloworld/application.py
@@ -20,7 +20,7 @@ def get_ip():
     print(get_ip_meta())
     return Response(json.dumps(format(get_ip_meta())), mimetype='application/json', status=200)
     
-@application.route('/temp/<temp>', methods=['GET']) #Class 7 send data to dynamodb
+@application.route('/temp/<temp>', methods=['POST']) #Class 7 send data to dynamodb
 def get_temp(temp):
     response = get_ip_meta()
     dynamodb = boto3.resource('dynamodb')
@@ -40,20 +40,31 @@ def get_temp(temp):
 
     return Response(json.dumps(item), mimetype='application/json', status=200)
     
-@application.route('/bi', methods=['GET']) #class 7 scan dynamo table
-def get_bi():
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('eb_try_logger')
+#@application.route('/bi', methods=['GET']) #class 7 scan dynamo table
+#def get_bi():
+ #   dynamodb = boto3.resource('dynamodb')
+  #  table = dynamodb.Table('eb_try_logger')
     # replace table scan
+   # resp = table.scan()
+
+    #return Response(json.dumps(str(resp)), mimetype='application/json', status=200)
+    #return render_template('index.html', response=str(resp), title='bi') #return html page
+    
+    
+@application.route('/bi', methods=['GET'])
+def get_bi():
+    my_ses = boto3.Session(region_name = 'us-east-2')
+    dynamodb = my_ses.resource('dynamodb')
+    table = dynamodb.Table('eb_try_logger')
     resp = table.scan()
 
     #return Response(json.dumps(str(resp)), mimetype='application/json', status=200)
-    return render_template('index.html', response=str(resp), title='bi') #return html page
+    return render_template('index.html', response=str(resp), title='bi')
 
 
-@application.route('/', methods=['POST'])
-def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+#@application.route('/', methods=['POST'])
+#def post():
+ #   return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
 
 def get_ip_meta(): #Class 4
